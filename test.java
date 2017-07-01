1568759caceb781655e0e0acb66b8c665481b76d
kcj8855 <kcj8855@hanmail.net>
2017-07-01 21:33:48 +0900

    Create test.java

---
+++ test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class add {
+	public static void main(String args[]){
+		int A, B;
+		Scanner s = new Scanner(System.in);
+		A = s.nextInt();
+		B = s.nextInt();
+		s.close();
+		
+		System.out.println(A+B);
+	}
+}
