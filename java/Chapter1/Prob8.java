d6f153f47108d5794931f40a72615c66ced41f5f
kcj8855 <kcj8855@hanmail.net>
2017-07-06 18:45:06 +0900

    챕터1 7,8번 수정

--- java/Chapter1/Prob8.java
+++ java/Chapter1/Prob8.java
@@ -1,15 +1,32 @@
-package BackJoon.Chapter1;
+package Baekjoon.Chapter1;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
- * Created by cjnote on 17. 6. 28.
+ * Created by CJdesktop on 2017-07-06.
+ *
+ * 입력 받은 대로 출력하는 프로그램을 작성하시오.
+ *
+ * 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다.
+ * 각 줄은 100글자를 넘지 않으며, 빈 줄이 주어질 수도 있고, 각 줄의 앞 뒤에 공백이 있을 수도 있다.
  */
 public class Prob8 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        while (s.hasNextLine()){
-            System.out.println(s.nextLine());
-        }
+        String temp;
+        String reg = "^[a-zA-Z0-9 ]*$";
+        int count = 0;
+        do {
+            temp = s.nextLine();
+            if(temp.isEmpty()){
+                continue;
+            }
+            else if (Pattern.matches(reg, temp)) {
+                System.out.println(temp);
+                count++;
+            }
+            System.out.println("count = " + count);
+        } while (count != 100);
     }
 }
