63493ce24936cd8690b0472f69e738b1f292d35e
kcj8855 <kcj8855@hanmail.net>
2017-07-06 19:27:05 +0900

    정렬

--- java/Chapter1/Prob7.java
+++ java/Chapter1/Prob7.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 /**
  * Created by CJdesktop on 2017-07-06.
  * 입력 받은 대로 출력하는 프로그램을 작성하시오.
- *
+ * <p>
  * 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다.
  * 각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다. 또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
  */
@@ -19,10 +19,9 @@ public class Prob7 {
         int count = 0;
         do {
             temp = s.nextLine();
-            if(temp.isEmpty()){
+            if (temp.isEmpty()) {
                 continue;
-            }
-            else if (temp.length()<101 && temp.charAt(0) != ' ' && temp.charAt(temp.length() - 1) != ' ' && Pattern.matches(reg, temp)) {
+            } else if (temp.length() < 101 && temp.charAt(0) != ' ' && temp.charAt(temp.length() - 1) != ' ' && Pattern.matches(reg, temp)) {
                 System.out.println(temp);
                 count++;
             }
--- java/Chapter1/Prob8.java
+++ java/Chapter1/Prob8.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 
 /**
  * Created by CJdesktop on 2017-07-06.
- *
+ * <p>
  * 입력 받은 대로 출력하는 프로그램을 작성하시오.
- *
+ * <p>
  * 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다.
  * 각 줄은 100글자를 넘지 않으며, 빈 줄이 주어질 수도 있고, 각 줄의 앞 뒤에 공백이 있을 수도 있다.
  */
@@ -19,15 +19,14 @@ public class Prob8 {
         int count = 0;
         do {
             temp = s.nextLine();
-            if(temp.isEmpty()){
+            if (temp.isEmpty()) {
                 System.out.println();
                 count++;
-            }
-            else if (temp.length()<101 && Pattern.matches(reg, temp)) {
+            } else if (temp.length() < 101 && Pattern.matches(reg, temp)) {
                 System.out.println(temp);
                 count++;
             }
+            System.out.println("count = " + count);
         } while (count != 100);
-        s.close();
     }
 }
